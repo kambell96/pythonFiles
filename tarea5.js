f6c96b0b9b7944983310cc58c948833b5cc449ee
kambell96 <camilomeneses1996@hotmail.com>
2018-01-04 16:04:11 -0500

    the missing tasks have been done

--- tarea5.js
+++ tarea5.js
@@ -9,59 +9,6 @@ var pesoC = prompt("Peso para esfera C");
 var pesoD = prompt("Peso para esfera D");
 
 
-// if (pesoA == pesoB && pesoA == pesoC && pesoA == pesoD); {
-// 	alert("las esferas son iguales")
-
-// 	if (pesoA == pesoB && pesoA == pesoC); {
-
-// 		if (); {
-// 			alert("D es distinta y mayor que las de mas esferas ")
-// 		}
-
-// 		if (pesoD < pesoA && pesoD < pesoB && pesoD < pesoC); {
-// 			alert("D es distinta y menor que las de mas esferas ")
-// 		}
-
-// 	}
-
-// 	if (pesoA == pesoB && pesoA == pesoD); {
-
-// 		if (pesoC > pesoA && pesoC > pesoB && pesoC > pesoD); {
-// 			alert("C es distinta y mayor que las de mas esferas ")
-
-// 		}
-// 		if (pesoC < pesoA && pesoC < pesoB && pesoC < pesoD); {
-// 			alert("C es distinta y menor que las de mas esferas ")
-// 		}
-
-// 	}
-
-// 	if (pesoA == pesoC && pesoA == pesoD); {
-
-// 		if (pesoB > pesoA && pesoB > pesoC && pesoB > pesoD); {
-// 			alert("B es distinta y mayor que las de mas esferas ")
-// 		}
-
-// 		if (pesoB < pesoA && pesoB < pesoC && pesoB < pesoD); {
-// 			alert("B es distinta y menor que las de mas esferas ")
-// 		}
-
-// 	}
-
-// 	if (pesoB == pesoC && pesoB == pesoD); {
-
-// 		if (pesoA > pesoB && pesoA > pesoC && pesoA > pesoD); {
-// 			alert("A es distinta y mayor que las de mas esferas ")
-// 		}
-
-// 		if (pesoA < pesoB && pesoA < pesoC && pesoA < pesoD); {
-// 			alert("A es distinta y menor que las de mas esferas ")
-// 		}
-
-// 	}
-
-// }
-
 
 if (pesoA == pesoB && pesoA == pesoC && pesoA == pesoD);
 {
